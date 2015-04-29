80aefd413e224f6d2fe6c6adbe6a3ce96750ff10
costerik <costerik@hotmail.com>
2015-04-28 19:15:26 -0500

    add authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :tasks
+
+	def full_name
+		name+' '+last_name
+	end
 end
